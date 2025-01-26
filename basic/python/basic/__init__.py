5828541ed21dd9ec47de69fd22c81d18441cd751
Mislav Novakovic <mislav.novakovic@gmail.com>
2025-01-26 20:26:50 +0200

    create basic maturin pyroject with pyo3
    Signed-off-by: Mislav Novakovic <mislav.novakovic@gmail.com>

---
+++ basic/python/basic/__init__.py
@@ -0,0 +1,6 @@
+from .basic import *
+
+
+__doc__ = basic.__doc__
+if hasattr(basic, "__all__"):
+    __all__ = basic.__all__
---
+++ basic/python/tests/test_all.py
@@ -0,0 +1,6 @@
+import pytest
+import basic
+
+
+def test_sum_as_string():
+    assert basic.sum_as_string(1, 1) == "2"
